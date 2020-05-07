d3250b71d168b02a09c38cb397bfb49baef5af16
max furman <mx.furman@gmail.com>
2020-05-07 13:56:59 -0700

    wip

--- acme/api/middleware.go
+++ acme/api/middleware.go
@@ -30,27 +30,23 @@ func logNonce(w http.ResponseWriter, nonce string) {
 	}
 }
 
-// baseURLFromRequest determines the base URL which should be used for constructing link URLs in e.g. the ACME directory
-// result by taking the request Host, TLS and Header[X-Forwarded-Proto] values into consideration.
-// If the Request.Host is an empty string, we return an empty string, to indicate that the configured
-// URL values should be used instead.
-// If this function returns a non-empty result, then this should be used in constructing ACME link URLs.
+// baseURLFromRequest determines the base URL which should be used for
+// constructing link URLs in e.g. the ACME directory result by taking the
+// request Host into consideration.
+//
+// If the Request.Host is an empty string, we return an empty string, to
+// indicate that the configured URL values should be used instead.  If this
+// function returns a non-empty result, then this should be used in
+// constructing ACME link URLs.
 func baseURLFromRequest(r *http.Request) string {
-	// TODO: I semantically copied the functionality of determining the protol from boulder web/relative.go
-	// which allows HTTP. Previously this was always forced to be HTTPS for absolute URLs. Should this be
-	// changed to also always force HTTPS protocol?
-	proto := "http"
-	if specifiedProto := r.Header.Get("X-Forwarded-Proto"); specifiedProto != "" {
-		proto = specifiedProto
-	} else if r.TLS != nil {
-		proto += "s"
-	}
+	// NOTE: See https://github.com/letsencrypt/boulder/blob/master/web/relative.go
+	// for an implementation that allows HTTP requests using the x-forwarded-proto
+	// header.
 
-	host := r.Host
-	if host == "" {
+	if r.Host == "" {
 		return ""
 	}
-	return proto + "://" + host
+	return "https://" + r.Host
 }
 
 // baseURLFromRequest is a middleware that extracts and caches the baseURL
